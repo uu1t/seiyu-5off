77259965e828586620c5e9fd4deedaa715f36409
Yuichi Tanikawa <kojole.jp@gmail.com>
2018-08-27 08:33:29 +0900

    Exit when failed to scrape

--- src/index.js
+++ src/index.js
@@ -2,10 +2,10 @@ const { scrape } = require('./scraper');
 const { write } = require('./writer');
 
 (async () => {
-  try {
-    const dates = await scrape();
-    write(dates);
-  } catch (err) {
-    console.error(err);
+  const dates = (await scrape().catch(err => console.error(err))) || [];
+  if (dates.length === 0) {
+    process.exit(1);
   }
+  console.log('Scrape', ...dates);
+  write(dates);
 })();
