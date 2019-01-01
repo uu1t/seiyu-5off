b78ae4468cd053e9ece4681da30522d62ed80438
Yuichi Tanikawa <u1.tanikawa@gmail.com>
2019-01-01 14:22:26 +0900

    Fix moving up a year in January

--- src/__test__/scraper.test.js
+++ src/__test__/scraper.test.js
@@ -2,19 +2,21 @@ const fetch = require('node-fetch')
 
 const scraper = require('../scraper')
 
-function html(month) {
-  const nextMonth = month === 12 ? 1 : month + 1
+function html(...months) {
   return `
 <div class="off_calendar">
   <div class="off_calendar_inner">
-    <ul class="off_calendar_list">
-      <li class="off_calendar_item"><span class="off_calendar_month">${month}/</span><span class="off_calendar_day">1</span><span class="off_calendar_week">（土）</span></li>
-      <li class="off_calendar_item"><span class="off_calendar_month">${month}/</span><span class="off_calendar_day">8</span><span class="off_calendar_week">（土）</span></li>
-    </ul>
-    <ul class="off_calendar_list">
-      <li class="off_calendar_item"><span class="off_calendar_month">${nextMonth}/</span><span class="off_calendar_day">2</span><span class="off_calendar_week">（土）</span></li>
-      <li class="off_calendar_item"><span class="off_calendar_month">${nextMonth}/</span><span class="off_calendar_day">9</span><span class="off_calendar_week">（土）</span></li>
-    </ul>
+    ${months
+      .map(
+        (month, i) => `
+        <ul class="off_calendar_list">
+          <li class="off_calendar_item"><span class="off_calendar_month">${month}/</span><span class="off_calendar_day">${i +
+          1}</span><span class="off_calendar_week">（土）</span></li>
+          <li class="off_calendar_item"><span class="off_calendar_month">${month}/</span><span class="off_calendar_day">${i +
+          8}</span><span class="off_calendar_week">（土）</span></li>
+        </ul>`
+      )
+      .join('')}
   </div>
 </div>
   `.trim()
@@ -27,30 +29,35 @@ describe('.scrape()', () => {
     fetch.resetMocks()
   })
 
-  it('extracts sale dates', async () => {
-    fetch.once(html(1)).once(html(11))
+  it('extracts sale dates in a month', async () => {
+    const months = [1, 2, 11, 12]
+    for (const month of months) {
+      fetch.once(html(month))
 
-    let dates = await scraper.scrape()
-    expect(dates).toEqual([
-      new Date(year, 1 - 1, 1),
-      new Date(year, 1 - 1, 8),
-      new Date(year, 2 - 1, 2),
-      new Date(year, 2 - 1, 9)
-    ])
-
-    dates = await scraper.scrape()
-    expect(dates).toEqual([
-      new Date(year, 11 - 1, 1),
-      new Date(year, 11 - 1, 8),
-      new Date(year, 12 - 1, 2),
-      new Date(year, 12 - 1, 9)
-    ])
+      const dates = await scraper.scrape(new Date(year, month - 1))
+      expect(dates).toEqual([new Date(year, month - 1, 1), new Date(year, month - 1, 8)])
+    }
   })
 
-  it('extracts sale dates in December', async () => {
-    fetch.once(html(12))
+  it('extracts sale dates in months', async () => {
+    const monthsList = [[1, 2], [2, 3], [11, 12]]
+    for (const months of monthsList) {
+      fetch.once(html(...months))
 
-    const dates = await scraper.scrape()
+      const dates = await scraper.scrape(new Date(year, months[0] - 1))
+      expect(dates).toEqual([
+        new Date(year, months[0] - 1, 1),
+        new Date(year, months[0] - 1, 8),
+        new Date(year, months[1] - 1, 2),
+        new Date(year, months[1] - 1, 9)
+      ])
+    }
+  })
+
+  it('extracts sale dates in December and January', async () => {
+    fetch.once(html(12, 1))
+
+    const dates = await scraper.scrape(new Date(year, 12 - 1))
     expect(dates).toEqual([
       new Date(year, 12 - 1, 1),
       new Date(year, 12 - 1, 8),
--- src/scraper.js
+++ src/scraper.js
@@ -6,9 +6,10 @@ const URL = 'https://www.seiyu.co.jp/service/5off/'
 /**
  * Scrape SEIYU 5 percent off sale dates.
  *
+ * @param {Date} [now]
  * @return {Promise<Date[]>}
  */
-async function scrape() {
+async function scrape(now = new Date()) {
   /** @type {Response} */
   const response = await fetch(URL)
   const text = await response.text()
@@ -22,11 +23,12 @@ async function scrape() {
     return { month, day }
   })
 
-  const maybeNewYear = dates.every(d => d.month === 1 || d.month === 12)
-  const currentYear = new Date().getFullYear()
+  // Handle dates in next year when it's in November and December
+  const maybeNewYear = now.getMonth() >= 10
+  const currentYear = now.getFullYear()
 
   return dates.map(d => {
-    const year = d.month === 1 && maybeNewYear ? currentYear + 1 : currentYear
+    const year = d.month <= 2 && maybeNewYear ? currentYear + 1 : currentYear
     return new Date(year, d.month - 1, d.day)
   })
 }
